88f0d3509f7b70b7169a9d47565c9a319e9514fb
RobinWassink <robin.wassink@uzh.ch>
2021-04-26 15:53:58 +0200

    tried ex2

---
+++ exercise_session_06/exercise_02/README.md
@@ -0,0 +1,2 @@
+# Exercise 2
+I tried implementing my own program, but I realised that computing the factorial is not the optimal solution as it's highly inefficient. 
---
+++ exercise_session_06/exercise_02/euler_debug.c
@@ -0,0 +1,37 @@
+#include <mpi.h>
+#include <stdio.h>
+
+int factorial(int n){
+ int fact=1;
+ for (int i=1; i<=n; i++){
+  fact *= i;
+ }
+ return fact;
+}
+
+int main(int argc, char *argv[]){
+ // init 
+ int n=8;
+ int size, myRank;
+
+ MPI_Init(&argc, &argv);
+ MPI_Comm_size(MPI_COMM_WORLD,&size);
+ MPI_Comm_rank(MPI_COMM_WORLD,&myRank);
+
+ // computation
+ double sum = 0.0;
+ double fact;
+ for (int i = myRank ; i < n; i += size) {
+  fact=factorial(i);
+  sum += 1/fact;
+  printf("sum is %.8f for rank %d\n", sum, myRank);
+ }
+
+ // reduce and output
+ double e=0;
+ MPI_Reduce(&sum, &e, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+ if(myRank==0){
+  printf("e is approximately %.16f", e);
+ }
+ MPI_Finalize();
+}
